a1547b54f5d63d0d53cb539cdecd799c081546d9
DamonLiu <damon.liu@xmight.com>
2024-10-28 15:02:37 +0800

    0 - init

---
+++ README.md
@@ -0,0 +1,4 @@
+# 「早知道就停紅線」 - 用 TDD 寫出一個會上新聞的停車場收費計算器
+
+[Youtube播放清單](https://youtube.com/playlist?list=PLvBh-90IwbPKFUUFw1PTezAVQqi0PUhTB&si=m_3gL1MK9nxfad6g)
+
---
+++ calculator.py
@@ -0,0 +1,6 @@
+class ParkingFeeCalculator:
+    """
+        Topic: 如何製作一個「能上新聞的」停車費計算機
+        https://youtube.com/playlist?list=PLvBh-90IwbPKFUUFw1PTezAVQqi0PUhTB&si=m_3gL1MK9nxfad6g
+    """
+    pass
