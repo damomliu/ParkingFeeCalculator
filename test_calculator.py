71d16aeabfae40b51ce29329a95587ccb7b7f4cc
DamonLiu <damon.liu@xmight.com>
2024-10-28 15:25:47 +0800

    refactor

--- test_calculator.py
+++ test_calculator.py
@@ -2,12 +2,15 @@ from datetime import datetime
 from calculator import ParkingFeeCalculator
 
 
-def test_15_mins_free():
-    c = ParkingFeeCalculator()
+PFC = ParkingFeeCalculator()
 
-    start = datetime(2024, 1, 1, 0, 0, 0)
-    end = datetime(2024, 1, 1, 0, 14, 59)
 
-    actual = c.calculate(start, end)
+class TestParkingFeeCalculator:
 
-    assert actual == 0
+    def test_15_mins_free(self):
+        start = datetime(2024, 1, 1, 0, 0, 0)
+        end = datetime(2024, 1, 1, 0, 14, 59)
+
+        actual = PFC.calculate(start, end)
+
+        assert actual == 0
